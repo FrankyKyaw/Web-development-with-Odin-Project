a0dfd22874be0c58bd6b6c5b41fe9aeb557a6643
FrankyKyaw <73188846+FrankyKyaw@users.noreply.github.com>
2022-06-23 15:28:18 -0400

    add images and work on front end

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,13 +1,33 @@
 console.log("Hello, World!")
 
+const choices = ["rock", "scissor", "paper"];
 function computerPlay(){
-    const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
-function playRound(playerSelection, computerSelection){
-
+/*
+function playRound(){
+    for (let i=0; i<1; i++) {
+        const computerSelection = computerPlay();
+        console.log("The computer picked", computerSelection);
+        
+        let playerSelection = prompt("Enter rock, paper or scissor: ");
+        let player = choices.indexOf(playerSelection);
+        let comp = choices .indexOf(computerSelection);
+        if ((player + 1) % 3 === comp){
+            console.log("You win!");
+        }
+        else if (comp === player){
+            console.log("It's a tie!");
+        }
+        else{
+            console.log("The computer wins!");
+        }
+    }
 }
 
-const computerSelection = computerPlay();
-console.log(computerPlay());
+
+array = ["a", "b", "c"];
+console.log(array.indexOf("a"));
+playRound();
+*/
