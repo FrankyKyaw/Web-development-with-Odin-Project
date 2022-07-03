e183512363bccf99ad54aeb8b6bc9ceb109e2b3a
FrankyKyaw <73188846+FrankyKyaw@users.noreply.github.com>
2022-07-03 18:32:59 -0400

    showing choices and scores

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,33 +1,81 @@
-console.log("Hello, World!")
 
 const choices = ["rock", "scissor", "paper"];
-function computerPlay(){
+let playerScore = 0
+let computerScore = 0
+let roundWinner = ''
+
+function getComputerSelection(){
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
-/*
-function playRound(){
+
+function playRound(playerSelection, computerSelection){
     for (let i=0; i<1; i++) {
-        const computerSelection = computerPlay();
-        console.log("The computer picked", computerSelection);
-        
-        let playerSelection = prompt("Enter rock, paper or scissor: ");
         let player = choices.indexOf(playerSelection);
         let comp = choices .indexOf(computerSelection);
         if ((player + 1) % 3 === comp){
-            console.log("You win!");
+            playerScore++;
         }
         else if (comp === player){
-            console.log("It's a tie!");
+
         }
         else{
-            console.log("The computer wins!");
+            computerScore++;
         }
     }
 }
 
 
-array = ["a", "b", "c"];
-console.log(array.indexOf("a"));
-playRound();
-*/
+
+
+function whenClicked(playerChoice){
+    const computerChoice = getComputerSelection();
+    updateChoices(playerChoice, computerChoice);
+    playRound(playerChoice, computerChoice);
+    updateScore();
+    
+}
+
+function updateChoices(playerChoice, computerChoice){
+    switch(playerChoice){
+        case 'rock':
+            playerIcon.textContent = '✊';
+            break;
+        case 'paper':
+            playerIcon.textContent = '✋';
+            break;
+        case 'scissor':
+            playerIcon.textContent = '✌';
+    }
+
+    switch(computerChoice){
+        case 'rock':
+            computerIcon.textContent = '✊';
+            break;
+        case 'paper':
+            computerIcon.textContent = '✋';
+            break;
+        case 'scissor':
+            computerIcon.textContent = '✌';
+    }
+    
+}
+
+function updateScore(){
+    player_score.textContent = `Player: ${playerScore}`;
+    computer_score.textContent = `Computer: ${computerScore}`;
+}
+
+const rockBtn = document.querySelector('.button#rock');
+const paperBtn = document.querySelector('.button#paper');
+const scissorBtn = document.querySelector('.button#scissor');
+const playerOptions = [rockBtn, paperBtn, scissorBtn];
+const playerIcon = document.querySelector('.player-icon');
+const computerIcon = document.querySelector('.computer-icon');
+const player_score = document.querySelector('.player-score');
+const computer_score = document.querySelector('.computer-score');
+
+
+rockBtn.addEventListener('click', () => whenClicked('rock'));
+paperBtn.addEventListener('click', () => whenClicked('paper'));
+scissorBtn.addEventListener('click', () => whenClicked('scissor'));
