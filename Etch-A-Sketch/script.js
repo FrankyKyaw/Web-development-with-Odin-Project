ea61e2a711eabe9942b5f3b0921627c413c6194a
FrankyKyaw <73188846+FrankyKyaw@users.noreply.github.com>
2022-07-14 15:45:02 -0400

    update grid size value

--- Etch-A-Sketch/script.js
+++ Etch-A-Sketch/script.js
@@ -1,14 +1,37 @@
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(16,1fr)"
-board.style.gridTemplateRows = "repeat(16,1fr)"
+
+const sizeText = document.querySelector('.sizeValue');
+const sizeSlider = document.querySelector('.sizeSlider');
 
 
-function initGrid(){
-  for (let i=0; i<256; i++){
+function initGrid(size){
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll('div');
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size},1fr)`; 
+  board.style.gridTemplateRows = `repeat(${size},1fr)`;
+
+  let gridSize = size * size;
+  for (let i = 0; i < gridSize; i++){
     let square = document.createElement('div');
     square.style.background = 'blue';
     board.insertAdjacentElement("beforeend", square);
   }
 }
 
-initGrid();
+initGrid(16);
+
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
+sizeSlider.onchange = (e) => updateGrid(e.target.value);
+
+function updateSizeValue(value){
+  sizeText.innerHTML = `${value} x ${value}`;
+}
+
+function updateGrid(value){
+  initGrid(value);
+}
+
+
+
+
+
